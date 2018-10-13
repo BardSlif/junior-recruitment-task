36456a6e9eaa91182bd06dcfa3cb070b0be64d7a
Bartosz Ślifirski <b.slifirski@gmail.com>
2018-10-13 10:40:20 +0200

    Add comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
+/*
+* Gulp watches changes in scss, js and html and automatically refreshes browser when
+* changes are made ...
+*/
 gulp.task("watch", () => {
   gulp.watch('./frontend/styles/scss/**.scss', ['styles']);
   gulp.watch("./frontend/*.html").on('change', browserSync.reload);
@@ -12,7 +16,10 @@ gulp.task("watch", () => {
   });
 });
 
-
+/* 
+* callback function for styles to create css file from preprocessor 
+* and add prefixes when needed for different browsers compatibility 
+*/
 gulp.task("styles", () => {
   gulp.src('./frontend/styles/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
